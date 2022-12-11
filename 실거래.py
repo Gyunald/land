5a9ca8e3c5d6408358b2a728ae872dd1817d7ff3
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-11 22:01:40 +0900

    Update 실거래.py

--- 실거래.py
+++ 실거래.py
@@ -94,7 +94,7 @@ try:
             st.dataframe(당월전체.style.background_gradient(subset=['거래금액', '면적', '건축'], cmap="Reds"))      
 
     st.success('GTX 운정신도시 오픈챗 🚅 https://open.kakao.com/o/gICcjcDb')
-    st.warning('참여코드 🎯 2023gtxa')
+    st.warning('참여코드 🍩 2023gtxa')
     
 except Exception as e:
     st.error('No data.😎')
