27693581702472e5ed1500e919ae291a125a1d92
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-11 18:42:04 +0900

    Update 실거래.py

--- 실거래.py
+++ 실거래.py
@@ -49,7 +49,7 @@ def api(date):
 file_1 = pd.read_csv(st.secrets.user_path,encoding='cp949')
 user_key = st.secrets.user_key
 
-st.info('🔍 검색어를 입력하거나 선택하세요'
+st.info('🔍 검색어를 입력하거나 선택하세요')
 c1,c2,c3 = st.columns([1,1,1])
 
 try:
