a67d3fe08d42f3285e05adcb435d84eacea974b8
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-12 18:21:12 +0900

    Update 실거래.py

--- 실거래.py
+++ 실거래.py
@@ -107,13 +107,13 @@ try:
         if len(당월전체) == 0 :
             st.info(f'{date[4:5+1]}월 신규 등록이 없습니다😎')
         else:
-            st.dataframe(아파트별.reset_index(drop=True).style.background_gradient(subset=['거래금액','면적','건축'], cmap='Reds')) 
+            st.table(아파트별.reset_index(drop=True).style.background_gradient(subset=['거래금액','면적','건축'], cmap='Reds')) 
             
     with st.expander(f'{시군구} 실거래 - {date[4:5+1]}월 전체',expanded=True) :
         if len(당월전체) == 0 :
             st.info(f'{date[4:5+1]}월 신규 등록이 없습니다😎')
         else:
-            st.dataframe(당월전체.style.background_gradient(subset=['거래금액', '면적', '건축'], cmap="Reds"))      
+            st.table(당월전체.style.background_gradient(subset=['거래금액', '면적', '건축'], cmap="Reds"))      
 
     st.success('GTX 운정신도시 오픈챗 🚅 https://open.kakao.com/o/gICcjcDb')
     st.warning('참여코드 🍩 2023gtxa')
