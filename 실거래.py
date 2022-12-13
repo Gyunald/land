01aba5c7132afd8def93ba9fb878a85d06260967
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-13 13:02:27 +0900

    Update 실거래.py

--- 실거래.py
+++ 실거래.py
@@ -93,7 +93,7 @@ try:
     오늘합['계약일'] = pd.to_datetime(오늘합['거래일'],format = "%Y%m%d").dt.strftime('%y.%m.%d')
     오늘합['거래금액'] = 오늘합['거래금액'].astype('int64')
     오늘합['면적'] = 오늘합['면적'].astype(float).map('{:.2f}'.format)
-    오늘합 = 오늘합[["아파트                    ", "거래금액", "층", "면적", "계약일","건축", "동"]].sort_values(by=['거래금액'], ascending=False).reset_index(drop=True)
+    오늘합 = 오늘합[["아파트                    ", "거래금액","면적","건축", "동"]].sort_values(by=['거래금액'], ascending=False).reset_index(drop=True)
     
     if 시군구:
         당월전체 = 오늘합
