ac1017c7ba7905248995be18c08d002634244683
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-23 08:58:08 +0900

    Update 실거래.py

--- 실거래.py
+++ 실거래.py
@@ -193,7 +193,7 @@ try:
     if 오늘 == date:
         with st.expander(f'{시군구} 실거래 - {date[6:]}일 🚀 신규 {len(신규)}건',expanded=True):
             st.info('신규거래😎')
-            st.dataframe(신규.style.background_gradient(subset=['금액', '면적', '계약'], cmap="Reds"))
+            st.dataframe(고정.style.background_gradient(subset=['금액', '면적', '계약'], cmap="Reds")) # 원래는 신규
             
     st.success('🚥 [GTX 운정신도시 정보공유](%s)' % 'https://open.kakao.com/o/gICcjcDb')
     st.warning('🚧 참여코드 : gtxa24')
