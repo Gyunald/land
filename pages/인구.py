40d668beeaa2acef256e4f29f2f8f35030874029
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-22 14:43:55 +0900

    Update 인구.py

--- pages/인구.py
+++ pages/인구.py
@@ -7,7 +7,7 @@ drop_colums = ['시점','등록인구.1','등록인구.2','등록인구.4','등�
 drop_indexs = ['읍면동별(1)']
 
 def csv_file(year):
-    a = pd.read_csv(st.secrets.population,index_col=1,encoding='cp949')
+    a = pd.read_csv(st.secrets.population,index_col=1)
     a = a.drop(drop_indexs,axis=0)
     a = a.drop(drop_colums,axis=1)
     a.rename(columns=rename_columns, index=rename_columns, inplace=True)
