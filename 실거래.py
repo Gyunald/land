8bd35fa3e5191816948faddcfe0888098c1dc53c
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-01-07 23:15:58 +0900

    Update 실거래.py

--- 실거래.py
+++ 실거래.py
@@ -182,21 +182,22 @@ try:
         with tab1 :
             if not 아파트:            
                 아파트별멀티 = 갱신
+                st.dataframe(아파트별멀티.style.background_gradient(subset=['금액','면적','계약'], cmap="Reds"),use_container_width=True)
             else:
                 아파트별멀티 = 갱신[갱신["아파트"].isin(아파트)].reset_index(drop=True)
-            st.error('📈 시세 동향')
-            chart = get_chart(아파트별멀티)
-            st.altair_chart(chart,use_container_width=True)
-        
-            st.dataframe(아파트별멀티.style.background_gradient(subset=['금액','면적','계약'], cmap="Reds"),use_container_width=True)
+                st.dataframe(아파트별멀티.style.background_gradient(subset=['금액','면적','계약'], cmap="Reds"),use_container_width=True)
+                st.error('📈 시세 동향')
+                chart = get_chart(아파트별멀티)
+                st.altair_chart(chart,use_container_width=True)
+
         with tab2:
             if 아파트:
                 당월_전세_전체 = 당월_전세_전체[당월_전세_전체["아파트"].isin(아파트)]
-            st.error('📈 시세 동향')
-            chart = get_chart(당월_전세_전체)
-            st.altair_chart(chart,use_container_width=True)
-            st.dataframe(당월_전세_전체.style.background_gradient(subset=['금액','면적','계약'], cmap="Reds"),use_container_width=True)
-               
+                st.dataframe(당월_전세_전체.style.background_gradient(subset=['금액','면적','계약'], cmap="Reds"),use_container_width=True)
+                st.error('📈 시세 동향')
+                chart = get_chart(당월_전세_전체)
+                st.altair_chart(chart,use_container_width=True)
+                 
         with tab3:
             if 아파트:
                 당월_월세_전체 = 당월_월세_전체[당월_월세_전체["아파트"].isin(아파트)]                
