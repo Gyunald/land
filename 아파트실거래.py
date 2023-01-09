27487bdb1bf2f1b766cb93689d6305087b82d3f3
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-01-10 07:21:58 +0900

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -203,7 +203,7 @@ try:
         
     오늘 = datetime.datetime.now().strftime('%Y%m%d')
     당월 = (datetime.datetime.now() + datetime.timedelta(hours=9)).strftime('%Y%m%d')
-    당월
+    
     # 당월 = datetime.datetime(year=int(date[:3 + 1]),month=int(date[4:5 + 1]),day=int(date[6:]))
     전월 = datetime.datetime.now() - datetime.timedelta(days=30)
     # 어제 = datetime.datetime.now() - datetime.timedelta(days=1)
@@ -211,8 +211,8 @@ try:
     갱신['금액'] = 갱신['금액'].astype(int)
     갱신 = 갱신.reindex(columns=["아파트", "금액", "층", "면적", "건축", "계약", "동", "거래", "파기"])
    
-    당월_매매_전체 = 갱신[갱신['계약'].str.contains(date_2)]    
-    전월당월전세월세 = pd.concat([api2(당월.strftime('%Y%m%d')),api2(전월.strftime('%Y%m%d'))]).reset_index(drop=True)
+    당월_매매_전체 = 갱신[갱신['계약'].str.contains(date_2)]
+    전월당월전세월세 = pd.concat([api2(당월),api2(전월)]).reset_index(drop=True)
     당월_전세_전체 = 전월당월전세월세[(전월당월전세월세['계약'].str.contains(date_2)) & (전월당월전세월세['월세'] == '0')].drop(columns=['월세']).reset_index(drop=True)
     당월_월세_전체 = 전월당월전세월세[(전월당월전세월세['계약'].str.contains(date_2)) & (전월당월전세월세['월세'] != '0')].reset_index(drop=True)
     
