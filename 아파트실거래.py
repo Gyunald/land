9b387f94212373d7a3960c6d84a544b1d77d9d58
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-01-10 07:36:41 +0900

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -203,7 +203,7 @@ try:
         
     # 오늘 = datetime.datetime.now().strftime('%Y%m%d')    
     당월 = datetime.datetime(year=int(date[:3 + 1]),month=int(date[4:5 + 1]),day=int(date[6:]))
-    전월 = 당월 - datetime.timedelta(days=30).strftime('%Y%m%d')
+    전월 = 당월 - datetime.timedelta(days=30)
     # 어제 = datetime.datetime.now() - datetime.timedelta(days=1)
     갱신 = pd.concat([api(당월.strftime('%Y%m%d')),api(전월.strftime('%Y%m%d'))]).reset_index(drop=True)
     갱신['금액'] = 갱신['금액'].astype(int)
