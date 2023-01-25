903a2b7d76c5ff72874035e952a6cd226b76de1f
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-01-25 23:11:16 +0900

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -44,7 +44,6 @@ def 매매(city, date, user_key, rows):
         aptTrade['아파트'] = aptTrade['아파트'].str.replace(i,'',regex=True)
         aptTrade['거래'] = aptTrade['거래'].str.replace(i,'',regex=True)
     aptTrade['금액'] = aptTrade['금액'].str.replace(',','').astype('int32')
-    aptTrade['파기'] = aptTrade['파기'].str.replace('22.','',regex=True)
     aptTrade['계약'] = pd.to_datetime(aptTrade['계약'],format = "%Y%m%d").dt.strftime('%y.%m.%d')
     aptTrade['면적'] = aptTrade['면적'].astype(float).map('{:.0f}'.format)
     aptTrade['동'] = aptTrade['동'].str.split().str[0]
@@ -87,8 +86,8 @@ def 임대(city, date, user_key, rows):
         aptTrade['아파트'] = aptTrade['아파트'].str.replace(i,'',regex=True)
     aptTrade['보증금'] = aptTrade['보증금'].str.replace(',','').astype('int32')
     aptTrade['계약'] = pd.to_datetime(aptTrade['계약'],format = "%Y%m%d").dt.strftime('%y.%m.%d')
-    aptTrade['종전보증금'] = aptTrade['종전보증금'].str.replace(',','').replace(' ','0').astype('int32')
-    aptTrade['종전월세'] = aptTrade['종전월세'].replace(' ','0').astype('int32')
+    aptTrade['종전보증금'] = aptTrade['종전보증금'].str.replace(',','',regex=True).replace(' ','0',regex=True).astype('int32')
+    aptTrade['종전월세'] = aptTrade['종전월세'].replace(' ','0',regex=True).astype('int32')
     aptTrade['면적'] = aptTrade['면적'].map('{:.0f}'.format)
     aptTrade['동'] = aptTrade['동'].str.split().str[0]
     aptTrade['층'] = aptTrade['층'].astype('int32')
