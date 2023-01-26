c747a79dae7e361e63e596162f7e855173dc0456
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-01-26 23:09:02 +0900

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -86,7 +86,6 @@ def 임대(city, date, user_key, rows):
         aptTrade['아파트'] = aptTrade['아파트'].str.replace(i,'',regex=True)
     aptTrade['보증금'] = aptTrade['보증금'].str.replace(',','').astype(int)
     aptTrade['계약'] = pd.to_datetime(aptTrade['계약'],format = "%Y%m%d").dt.strftime('%y.%m.%d')
-    aptTrade['월세'] = aptTrade['월세'].str.replace(',','',regex=True).replace(' ','0',regex=True).astype(int)
     aptTrade['종전보증금'] = aptTrade['종전보증금'].str.replace(',','',regex=True).replace(' ','0',regex=True).astype(int)
     aptTrade['종전월세'] = aptTrade['종전월세'].replace(' ','0',regex=True).astype(int)
     aptTrade['면적'] = aptTrade['면적'].map('{:.0f}'.format).astype(int)
