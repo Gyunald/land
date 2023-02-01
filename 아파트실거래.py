15b24e2dc3416e170c9e70462f36280389203b45
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-01 19:07:03 +0900

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -171,7 +171,18 @@ def 실거래(url, city, date, user_key, rows):
     return aptTrade.sort_values(by=['아파트'], ascending=True)
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate('kdongsan.json')
+    cred = credentials.Certificate({
+    "type": st.secrets.type,
+    "project_id": st.secrets.project_id,
+    "private_key_id": st.secrets.private_key_id,
+    "private_key": st.secrets.private_key,
+    "client_email": st.secrets.client_email,
+    "client_id": st.secrets.client_id,
+    "auth_uri": st.secrets.auth_uri,
+    "token_uri": st.secrets.token_uri,
+    "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
+    "client_x509_cert_url": st.secrets.client_x509_cert_url
+    })
     app = firebase_admin.initialize_app(cred)
     
 db = firestore.client()
