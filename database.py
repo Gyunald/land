eeb61a1891607717b814080250c0982c1dc6fa42
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-17 06:49:53 +0900

    Update database.py

--- database.py
+++ database.py
@@ -81,8 +81,8 @@ if login_code == st.secrets.login_code :
     
     db = firestore.client()
 
-    urls= {'trade' : st.secrets.api_path,'rent' : st.secrets.api_path_2}
-    #urls={'rent' : st.secrets.api_path_2}
+    #urls= {'trade' : st.secrets.api_path,'rent' : st.secrets.api_path_2}
+    urls={'rent' : st.secrets.api_path_2}
     file_1 = pd.read_csv(st.secrets.user_path,encoding='cp949')
     user_key = st.secrets.user_key
     rows = '9999'
@@ -93,14 +93,15 @@ if login_code == st.secrets.login_code :
     if 당월.day == 1 :
         당월 = 당월 - timedelta(days=1)
         전월 = 당월.replace(day=1) - timedelta(days=1)
-
+    d1=50
+    d2=-1
     c = 0
-    if not db.collection(f"{당월.strftime('%d')}_trade_{당월.strftime('%y.%m')}").document('서울특별시 종로구').get().exists:
+    if db.collection(f"{당월.strftime('%d')}_trade_{당월.strftime('%y.%m')}").document('서울특별시 종로구').get().exists:
         for i,j in urls.items():
             당월합= pd.DataFrame()
             전월합= pd.DataFrame()
             start = datetime.utcnow()+timedelta(hours=9)
-            for city,dong in zip(file_1['법정동코드'].astype(str).str[:5],file_1['법정동명']):
+            for city,dong in zip(file_1['법정동코드'][d1:d2].astype(str).str[:5],file_1['법정동명'][d1:d2]):
                 합_당월매매 = {}
                 st.write(f"{c}/250 {dong} complete...")
                 당월매매 = 실거래(j, city, 당월.strftime('%Y%m'), user_key, rows, dong)
