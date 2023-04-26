17b1d6c2731037b29075183ccbe3e89223c7c37b
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-04-27 07:12:38 +0900

    Update database.py

--- database.py
+++ database.py
@@ -100,7 +100,7 @@ if login_code == st.secrets.login_code :
     c = 0
     d = 0
 
-    if not db.collection(f"{당월.strftime('%Y.%m.%d')}").document('서울특별시 종로구').get().exists:
+    if not db.collection(f"{당월.strftime('%Y.%m.')}").document('서울특별시 종로구').get().exists:
         for dong,code in address.items():        
             tread_1 = Thread(target=실거래, args=(urls['매매'], code, user_key, rows, dong,'매매'))
             tread_2 = Thread(target=실거래, args=(urls['임대'], code, user_key, rows, dong,'임대'))
