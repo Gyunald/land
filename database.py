438e6e94922ecec11f5a2c79e658669ae92a1579
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-07-06 08:29:06 +0900

    Update database.py

--- database.py
+++ database.py
@@ -111,8 +111,7 @@ if choice == '업데이트' :
                     tread_1.start()
                     tread_2.start()
                     c += (100/len(address))
-                    bar = st.progress(0)
-                    empty2.progress(int(c)+1)
+                    empty2.progress(int(c))
                 empty.warning('업데이트 완료')
 
             else:
