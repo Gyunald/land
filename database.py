2aa7d6e344b4550935114949a7d32c4896d16a90
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-08-26 11:04:11 +0900

    Update database.py

--- database.py
+++ database.py
@@ -145,7 +145,7 @@ if choice == '삭제':
     login_code2 = empty.text_input('삭제 코드 ', type='password')
 
     if login_code2 == st.secrets.login_code :
-        for i in list(db.collections())[-1:]:
+        for i in list(db.collections())[-1 :]:
             c = 0
             db = firestore.client()
             db = db.collection(i.id).get()
