1226870a0f826d1c8651aca3386aac2e63cb9126
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-09-13 20:17:40 +0900

    Update paju.py

--- paju.py
+++ paju.py
@@ -13,12 +13,12 @@ def 정규화(신규):
     temp['계약'] = pd.to_datetime(temp['계약'],format = "%Y%m%d").dt.strftime('%m.%d')
     temp['면적'] = temp['면적'].astype(float).map('{:.0f}'.format)
     temp['동'] = temp['동'].str.split().str[0]
-    temp['금액'] = (temp['금액'].astype(float) / 10000).astype('float')
-    replace_word = '\(.+\)',city_replace,'아파트','마을','신도시','단지','국제금융'
+    temp['금액'] = (temp['금액'].astype(float) / 10000)
+    replace_word = '\(.+\)',city_replace,'아파트','마을','신도시','단지'
     for i in replace_word:
         temp['아파트'] = temp['아파트'].str.replace(i,'',regex=True)
 
-    return temp.sort_values(by=['아파트'], ascending=True)
+    return temp
 
 if not firebase_admin._apps:
     cred = credentials.Certificate({
@@ -36,7 +36,8 @@ if not firebase_admin._apps:
     app = firebase_admin.initialize_app(cred)
     
 db = firestore.client()
-cities =  ['파주시', '김포시', '고양시 일산서구', '고양시 덕양구', '인천광역시 연수구', '남양주시', '하남시', '성남시 분당구', '수원시 영통구', '용인시 수지구', '화성시', '평택시']
+cities =  ['파주시', '김포시', '고양시 일산서구', '고양시 일산동구', '인천광역시 연수구', '인천광역시 서구', '성남시 분당구', '수원시 영통구', '용인시 수지구', '화성시', '평택시']
+
 for city in cities[::-1]:
     if list(db.collections())[-1].id == (datetime.utcnow()+timedelta(hours=9)).date().strftime('%Y.%m.%d') :        
         매매 = db.collection(list(db.collections())[-1].id).document(city).get().to_dict()['매매']
@@ -48,9 +49,9 @@ for city in cities[::-1]:
         if len(신규) >= 1:
             e1 = st.empty()
             e = st.empty()
-            e1.write(f'{city} {(datetime.utcnow()+timedelta(hours=9)).day}일 - 신규 {len(신규)}건')
+            e1.write(f'### {city} {(datetime.utcnow()+timedelta(hours=9)).day}일 - 신규 {len(신규)}건')
             float_point = dict.fromkeys(신규.select_dtypes('float').columns, "{:.1f}")
             e.dataframe(신규.sort_values(by=['금액'], ascending=False).style.format(float_point).background_gradient(subset=['금액','층'], cmap='Reds'),use_container_width=True,hide_index=True)
-            time.sleep(2.9)
+            time.sleep(.1)
         e.empty()
         e1.empty()
