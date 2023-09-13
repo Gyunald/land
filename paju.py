c0f3e3835acb076c3afae64069d7e61f82aa6a9b
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-09-13 11:32:00 +0900

    Update paju.py

--- paju.py
+++ paju.py
@@ -16,7 +16,7 @@ def 정규화(신규):
     temp['동'] = temp['동'].str.split().str[0]
     temp['금액'] = (temp['금액'].astype(int) / 10000).astype(str)
     
-    replace_word = '\(.+\)','아파트','마을','신도시','단지','국제금융'
+    replace_word = '\(.+\)','아파트','마을','신도시','단지'
     for i in replace_word:
         temp['아파트'] = temp['아파트'].str.replace(i,'',regex=True)
     return temp.sort_values(by=['아파트'], ascending=True)
