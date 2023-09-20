b81b62901fb221c5d505bebf03cbd11d6b877256
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-09-20 13:53:19 +0900

    Update paju.py

--- paju.py
+++ paju.py
@@ -10,7 +10,7 @@ st.markdown('''
 <style>
 .stApp [data-testid="stHeader"] {visibility: hidden;}
 div[class^='block-container'] { padding-top: 1rem; }
-div[class^='block-container'] { padding-left: 2rem; }
+div[class^='block-container'] { padding-left: 1.5rem; }
 </style>
 ''', unsafe_allow_html=True)
 
