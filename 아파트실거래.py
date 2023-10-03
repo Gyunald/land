740d6bb3e84f7a279821f7078626da9a4361aad0
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-10-03 13:05:01 +0000

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -108,7 +108,7 @@ try:
             매매_당월 = 매매_당월[매매_당월["아파트"].isin(아파트)]
 
         float_point = dict.fromkeys(매매_당월.select_dtypes('float').columns, "{:.1f}")
-        매매_당월["Link"] = "https://"+매매_당월["아파트"]
+        매매_당월["지도"] = "https://map.naver.com/p/search/"+매매_당월["아파트"]
         e2.dataframe(매매_당월.sort_values(by=['금액'], ascending=False).style.format(float_point).background_gradient(subset=['금액','층'], cmap="Reds"),use_container_width=True,hide_index=True)
 
 except Exception as e:
