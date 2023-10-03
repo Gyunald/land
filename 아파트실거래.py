f9d2443a60ca1faeddfb286a7f8fcd44a6d17e1a
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-10-03 12:10:13 +0000

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -11,11 +11,7 @@ st.set_page_config(page_title="🎈아파트 실거래가") # layout='wide'
 
 st.markdown('''
 <style>
-    div[data-testid="manage-app-button"] {
-    visibility: hidden;
-    height: 0%;
-    position: fixed;
-    }
+    div[class="styles_terminalButton__JBj5T" {visibility: hidden;}
     
     div[class^='block-container'] { padding-top: 1rem; }
     
