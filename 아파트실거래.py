e0483acf9282729295b5df25250d5d6fdd0760be
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-10-03 13:43:16 +0000

    Update 아파트실거래.py

--- 아파트실거래.py
+++ 아파트실거래.py
@@ -108,15 +108,13 @@ try:
             매매_당월 = 매매_당월[매매_당월["아파트"].isin(아파트)]
 
         float_point = dict.fromkeys(매매_당월.select_dtypes('float').columns, "{:.1f}")
-        # 매매_당월["지도"] = "https://map.naver.com/p/search/"+매매_당월["아파트"]
+        매매_당월["아파트"] = "https://map.naver.com/p/search/"+매매_당월["아파트"]
         매매_당월 = 매매_당월.sort_values(by=['금액'], ascending=False)
         # e2.dataframe(매매_당월.sort_values(by=['금액'], ascending=False).style.format(float_point).background_gradient(subset=['금액','층'], cmap="Reds"),use_container_width=True,hide_index=True)
         
-        e2.data_editor(매매_당월.style.format(float_point).background_gradient(subset=['금액','층'], cmap="Reds"),use_container_width=True,hide_index=True,
+        e2.dataframe(매매_당월.style.format(float_point).background_gradient(subset=['금액','층'], cmap="Reds"),use_container_width=True,hide_index=True,
                          column_config={
-                             "아파트": st.column_config.LinkColumn(
-                                 validate = "https://map.naver.com/p/search/"
-                             )
+                             "아파트": st.column_config.LinkColumn()
                             }
                          )
 except Exception as e:
