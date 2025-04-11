ba1c36470a41bc182d6f43ca07aaa0bd74badeb1
Gyunald <97107396+Gyunald@users.noreply.github.com>
2025-04-11 11:06:34 +0900

    Update gold.py

--- gold.py
+++ gold.py
@@ -115,36 +115,36 @@ import streamlit as st
 BACKGROUND_COLOR = 'white'
 COLOR = 'black'
 
-def set_page_container_style(
-        max_width: int = 1100, max_width_100_percent: bool = False,
-        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
-        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
-    ):
-        if max_width_100_percent:
-            max_width_str = f'max-width: 100%;'
-        else:
-            max_width_str = f'max-width: {max_width}px;'
-        st.markdown(
-            f'''
-            <style>
-                .reportview-container .sidebar-content {{
-                    padding-top: {padding_top}rem;
-                }}
-                .reportview-container .main .block-container {{
-                    {max_width_str}
-                    padding-top: {padding_top}rem;
-                    padding-right: {padding_right}rem;
-                    padding-left: {padding_left}rem;
-                    padding-bottom: {padding_bottom}rem;
-                }}
-                .reportview-container .main {{
-                    color: {color};
-                    background-color: {background_color};
-                }}
-            </style>
-            ''',
-            unsafe_allow_html=True,
-        )
+
+max_width: int = 1100, max_width_100_percent: bool = False,
+padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
+color: str = COLOR, background_color: str = BACKGROUND_COLOR,
+
+if max_width_100_percent:
+    max_width_str = f'max-width: 100%;'
+else:
+    max_width_str = f'max-width: {max_width}px;'
+st.markdown(
+    f'''
+    <style>
+        .reportview-container .sidebar-content {{
+            padding-top: {padding_top}rem;
+        }}
+        .reportview-container .main .block-container {{
+            {max_width_str}
+            padding-top: {padding_top}rem;
+            padding-right: {padding_right}rem;
+            padding-left: {padding_left}rem;
+            padding-bottom: {padding_bottom}rem;
+        }}
+        .reportview-container .main {{
+            color: {color};
+            background-color: {background_color};
+        }}
+    </style>
+    ''',
+    unsafe_allow_html=True,
+)
 # st.markdown('''
 # <style>
 # .stApp [data-testid="stHeader"] {visibility: hidden;}
@@ -287,5 +287,4 @@ def main():
                 st.write(f"- 다이아몬드 중량: {diamond_weight:.2f}캐럿 ({diamond_weight*0.2:.2f}g)")
 
 if __name__ == "__main__":
-    set_page_container_style()
     main()
