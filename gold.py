0da2e8a4e83086372f50b1bbdc06c9bd59d9cfe8
Gyunald <97107396+Gyunald@users.noreply.github.com>
2025-04-11 10:31:52 +0900

    Update gold.py

--- gold.py
+++ gold.py
@@ -1,3 +1,36 @@
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # import requests
 # from bs4 import BeautifulSoup
 # import pandas as pd
