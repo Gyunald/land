2dd82187371ced2e3b49f3e315966760b25fa707
Gyunald <97107396+Gyunald@users.noreply.github.com>
2025-04-11 10:36:57 +0900

    Update gold.py

--- gold.py
+++ gold.py
@@ -114,7 +114,7 @@ import pandas as pd
 import streamlit as st
 hide_st_style = """
 
-#MainMenu {visibility: hidden;}
+MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
 
