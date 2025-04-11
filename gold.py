b347c31dd82bdae73f8291e530f3ca41ff3db2d7
Gyunald <97107396+Gyunald@users.noreply.github.com>
2025-04-11 11:03:52 +0900

    Update gold.py

--- gold.py
+++ gold.py
@@ -112,13 +112,45 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import streamlit as st
+BACKGROUND_COLOR = 'white'
+COLOR = 'black'
 
-st.markdown('''
-<style>
-.stApp [data-testid="stHeader"] {visibility: hidden;}
-div[class^='block-container'] { padding-top: 1rem; }
-</style>
-''', unsafe_allow_html=True)
+def set_page_container_style(
+        max_width: int = 1100, max_width_100_percent: bool = False,
+        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
+        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
+    ):
+        if max_width_100_percent:
+            max_width_str = f'max-width: 100%;'
+        else:
+            max_width_str = f'max-width: {max_width}px;'
+        st.markdown(
+            f'''
+            <style>
+                .reportview-container .sidebar-content {{
+                    padding-top: {padding_top}rem;
+                }}
+                .reportview-container .main .block-container {{
+                    {max_width_str}
+                    padding-top: {padding_top}rem;
+                    padding-right: {padding_right}rem;
+                    padding-left: {padding_left}rem;
+                    padding-bottom: {padding_bottom}rem;
+                }}
+                .reportview-container .main {{
+                    color: {color};
+                    background-color: {background_color};
+                }}
+            </style>
+            ''',
+            unsafe_allow_html=True,
+        )
+# st.markdown('''
+# <style>
+# .stApp [data-testid="stHeader"] {visibility: hidden;}
+# div[class^='block-container'] { padding-top: 1rem; }
+# </style>
+# ''', unsafe_allow_html=True)
 
 # 세션 상태 초기화
 if 'gold_price' not in st.session_state:
